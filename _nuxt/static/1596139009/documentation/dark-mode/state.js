9bc65c2bb569746d0a6a49f837618fbfcd1d7cd2
Mazel <mazuel.loic@gmail.com>
2020-07-30 21:56:59 +0200

    b84cfd7c

---
+++ _nuxt/static/1596139009/documentation/dark-mode/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1596139009",layout:"documentation",error:null,state:{darkTheme:false,state:{},mutations:{},getters:{},actions:{}},serverRendered:true,routePath:"\u002Fdocumentation\u002Fdark-mode",config:{}};
